46e7fbd00c302b8fff851bdf7fae144285c12012
ChimpanJP <your.email@example.com>
2022-11-30 13:48:57 +0000

    ユーザ登録作成中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
-  
-    def show
-      @user = User.find(params[:id])
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
     end
-  
-    def new
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
 end
